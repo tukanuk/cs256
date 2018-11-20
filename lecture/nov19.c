e89052a22cd7d5d602191e87a21f8fc14379ec67
Ben Davidson <benjamin@bMBA.local>
2018-11-20 10:46:15 -0500

    Nov19 lecture

---
+++ lecture/nov19.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <stdio.h> // This is the server
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/signal.h>
+// #include <sys/signal.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  
+}
